744cc7b0dccad77e7133b57e0c9b0bf9b11ddf2e
AyanamiReiEva <1142684462@qq.com>
2026-04-10 09:58:49 +0800

    feat: update home page to use async functions

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { getAllPosts } from "@/lib/posts";
 import { HomeClient } from "@/components/home-client";
 import type { PostMetadata } from "@/lib/posts";
 
-export default function Home() {
-  const posts = getAllPosts();
+export default async function Home() {
+  const posts = await getAllPosts();
   return <HomeClient posts={posts} />;
 }
